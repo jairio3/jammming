9e6e5a186ff6ae3b148e663bb4d9ad33a64858e5
unknown <jaimerios@tutamail.com>
2021-01-20 13:04:35 -0800

    render playlist tracks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,24 +4,36 @@ import { SearchBar } from "../SearchBar/SearchBar.js";
 import { SearchResults } from "../SearchResults/SearchResults.js";
 import { Playlist } from "../Playlist/Playlist.js";
 
-const TRACK = {
-  name: 'The Bay',
-  artist: 'Metronomy',
-  album: 'The English Riviera'
-}
+const TRACK_1 = {
+  name: "The Bay",
+  artist: "Metronomy",
+  album: "The English Riviera",
+};
 
-const SEARCHRESULTS = [TRACK, TRACK, TRACK, TRACK, TRACK]
+const TRACK_2 = {
+  name: "The Look",
+  artist: "Metronomy",
+  album: "The English Riviera",
+};
+
+const PLAYLIST_NAME = "Jam's Playlist";
+const SEARCH_RESULTS = [TRACK_1, TRACK_1, TRACK_1, TRACK_1, TRACK_1];
+const PLAYLIST_TRACKS = [TRACK_2, TRACK_2, TRACK_2, TRACK_2, TRACK_2];
 
 export class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      searchResults: SEARCHRESULTS
+      searchResults: SEARCH_RESULTS,
+      playListName: PLAYLIST_NAME,
+      playlistTracks: PLAYLIST_TRACKS,
     };
   }
 
   render() {
+    const searchResults = this.state.searchResults;
+    const playlistTracks = this.state.playlistTracks;
+    const playlistName = this.state.playlistName;
     return (
       <div>
         <h1>
@@ -30,8 +42,11 @@ export class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults}/>
-            <Playlist />
+            <SearchResults searchResults={searchResults} />
+            <Playlist
+              playListName={playlistName}
+              playlistTracks={playlistTracks}
+            />
           </div>
         </div>
       </div>
